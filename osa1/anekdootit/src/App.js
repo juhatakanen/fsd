2734a71c1fb10cdc32eaf6196b6698ba8acb403f
Juha Takanen <juhatakanen@hotmail.com>
2021-11-04 19:16:48 +0200

    1.13* tehty

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react'
 
-const Button = ({ handleClick }) => {
-  return (
-    <div>
-      <button onClick={handleClick}>next anecdote</button>
-    </div>
-  )
-}
 
 const App = () => {
   const anecdotes = [
@@ -15,17 +8,51 @@ const App = () => {
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code aleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
-  const [selected, setSelected] = useState(Math.floor(Math.random() * 6) + 1)
-
+  let random = Math.floor(Math.random() * 7)
+  console.log('random before correction', {random})
+  const [selected, setSelected] = useState(Math.floor(Math.random() * 7))
+  if (random === selected) {
+    random++
+    if (random === 7) {
+      random-=2
+    }
+    if (random === -1) {
+      random+=2
+    }
+  }
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0])
+  console.log('selected:', {selected})
+  console.log('random', {random})
+  console.log(votes)
   return (
     <div>
-      {anecdotes[selected]}
-      <Button handleClick={() => setSelected(Math.floor(Math.random() * 6) + 1)}/>
+      {anecdotes[selected]}<br />
+      has {votes[selected]} votes
+      <div>
+      <Button handleClick={() => {
+
+        const copy = [
+          ...votes
+        ]
+        copy[selected]++
+        setVotes(copy)
+        }
+      }
+      text='vote'/>
+      <Button handleClick={() => setSelected(random)} text='next anecdote'/>
+      </div>
     </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
   )
 }
 
--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -33,6 +33,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   return (
     <div>
       <h1>give feedback</h1>
