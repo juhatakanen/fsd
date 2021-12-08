e3a4d35dea87d0c722d256efd78ac93991920d51
Juha Takanen <juhatakanen@hotmail.com>
2021-12-08 18:14:57 +0200

    3.5 tehty

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -54,33 +54,26 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
+const generateId = () => {
+  return Math.floor(Math.random() * 100000000)
+}
 
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
+app.post('/api/persons', (request, response) => {
+  const personToAdd = request.body
+  if (!personToAdd.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
 
-//   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
-//     })
-//   }
-
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     date: new Date(),
-//     id: generateId(),
-//   }
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
+  const person = {
+    name: personToAdd.name,
+    number: personToAdd.number,
+    id: generateId(),
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
