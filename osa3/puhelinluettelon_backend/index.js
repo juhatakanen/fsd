23792aaa807209face06efc4c6e3d6dd4e621cd3
Juha Takanen <juhatakanen@hotmail.com>
2021-12-08 17:29:48 +0200

    3.2 tehty

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -26,8 +26,10 @@ let persons = [
           }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+app.get('/info', (req, res) => {
+  const date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`)
 })
 
 app.get('/api/persons', (req, res) => {
